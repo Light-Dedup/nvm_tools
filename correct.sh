aed46f83d0044518cec590fb57b716f65c8069c5
JianshengQIU <searchstar@localhost.localdomain>
2020-09-26 18:52:30 +0800

    Suggestion of remount

--- correct.sh
+++ correct.sh
@@ -3,8 +3,12 @@ if [ ! $3 ]; then
 	echo Usage: $0 num_of_threads size duplicate_percentage
 	exit
 fi
+if [ -r pmem/test1 ]; then
+	echo Please remount /mnt/pmem
+	exit
+fi
 TESTDIR=/home/searchstar/test
-rm $TESTDIR/test*
+rm -f $TESTDIR/test*
 TMPFILE=$(mktemp)
 for i in $(seq 1 $1); do
 	fio -filename=$TESTDIR/test$i -randseed=$i -direct=1 -iodepth 1 -rw=write -ioengine=psync -bs=4K -runtime=10 -thread -numjobs=1 -size=$2 -name=randrw --dedupe_percentage=$3 -group_reporting >> $TMPFILE &
@@ -23,6 +27,7 @@ cat $TMPOUT | sed 's/WRITE: bw=//g' | sed 's/MiB.*//g' | ./get_sum
 echo MiB/s
 
 rm $TMPFILE $TMPOUT
+sudo cp $TESTDIR/test* ./pmem/
 for i in $(seq 1 $1); do
 	cmp $TESTDIR/test$i ./pmem/test$i
 done
