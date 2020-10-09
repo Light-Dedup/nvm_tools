0e92939d3e23d154df5867710449e5e8ce88ba38
Jiansheng QIU <632863986@qq.com>
2020-10-09 15:21:41 +0800

    Convert to GiB/s

--- raw_perf/table.sh
+++ raw_perf/table.sh
@@ -1,13 +1,12 @@
-#TEMPOUT=$(mktemp)
-TEMPOUT=out.txt
-echo "threads READ WRITE"
+TEMPOUT=$(mktemp)
+echo "threads READ(GiB/s) WRITE(GiB/s)"
 for i in $(seq 1 $1); do
 	bash raw_perf.sh $i &> $TEMPOUT
 	echo -n "$i "
-	grep READ: $TEMPOUT | sed 's/.*READ: bw=//g' | sed 's/MiB.*//g' | tr -d "\n"
+	grep READ: $TEMPOUT | sed 's/.*READ: bw=//g' | sed 's/iB.*//g' | ./M2G | tr -d "\n"
 	echo -n " "
-	grep WRITE: $TEMPOUT | sed 's/.*WRITE: bw=//g' | sed 's/MiB.*//g' | tr -d "\n"
+	grep WRITE: $TEMPOUT | sed 's/.*WRITE: bw=//g' | sed 's/iB.*//g' | ./M2G | tr -d "\n"
 	echo
 done
-#rm $TEMPOUT
+rm $TEMPOUT
 
