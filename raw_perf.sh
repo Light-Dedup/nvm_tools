c00b7887322867b62106c186582ef5b82b4c945d
Jiansheng QIU <632863986@qq.com>
2020-10-09 13:54:00 +0800

    Fix

--- raw_perf.sh
+++ raw_perf.sh
@@ -1,6 +1,10 @@
 cd /mnt
 sudo bash init_ext4.sh
 cd pmem
-sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=write -ioengine=psync -bs=4K -thread -numjobs=1 -size=2G -name=raw_write --dedupe_percentage=0 -group_reporting
-sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=read -ioengine=psync -bs=4K -thread -numjobs=1 -size=2G -name=raw_read --dedupe_percentage=0 -group_reporting
+
+# Warm up
+sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=write -ioengine=mmap -bs=2G -thread -numjobs=1 -size=2G -name=raw_write --dedupe_percentage=0 -group_reporting &> /dev/null
+
+sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=write -ioengine=mmap -bs=2G -thread -numjobs=1 -size=2G -name=raw_write --dedupe_percentage=0 -group_reporting
+sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=read -ioengine=mmap -bs=2G -thread -numjobs=1 -size=2G -name=raw_read --dedupe_percentage=0 -group_reporting
 
