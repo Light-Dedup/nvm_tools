2320c2384e78992ed02f0aea9fad7da20058a293
Jiansheng QIU <qiujiansheng@stu.hit.edu.cn>
2020-11-03 15:46:16 +0800

    Echo to dmesg

--- make_table.sh
+++ make_table.sh
@@ -2,6 +2,7 @@ if [ ! $3 ]; then
 	echo Usage: $0 max_threads size step_of_dup_rate
 	exit
 fi
+sudo bash -c "echo make_table $* > /dev/kmsg"
 dup_arr=( $(seq 0 $3 100) )
 echo dup_rate numjobs throughput\(GiB/s\)
 for dup in ${dup_arr[@]}; do
