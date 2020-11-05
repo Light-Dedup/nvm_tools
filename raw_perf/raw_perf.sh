ac388ec6d2eb1251c9f4483fe699b4144ac3df9d
Jiansheng QIU <632863986@qq.com>
2020-11-05 12:33:12 +0800

    Support specify fio path when measuring raw performance.

--- raw_perf/raw_perf.sh
+++ raw_perf/raw_perf.sh
@@ -1,11 +1,11 @@
 set -e
 
-if [ ! $4 ]; then
-	echo Usage: $0 path_to_init_scirpt num_of_threads size read_rate
+if [ ! $5 ]; then
+	echo Usage: $0 path_to_fio path_to_init_scirpt num_of_threads size read_rate
 	exit
 fi
 sudo bash -c "echo $0 $* > /dev/kmsg"
-sudo bash $1
+sudo bash $2
 
-sudo fio -directory=/mnt/pmem -direct=1 -iodepth 1 -rw=rw -ioengine=mmap -bs=4K -thread -numjobs=$2 -size=$3 -name=rw --rwmixread=$4 --dedupe_percentage=0 -group_reporting
+sudo $1 -directory=/mnt/pmem -direct=1 -iodepth 1 -rw=rw -ioengine=mmap -bs=4K -thread -numjobs=$3 -size=$4 -name=rw --rwmixread=$5 --dedupe_percentage=0 -group_reporting
 
