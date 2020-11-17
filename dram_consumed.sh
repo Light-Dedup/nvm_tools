b594b05080539a430fb4030ac4c771bdd7629574
Jiansheng QIU <qiujiansheng@stu.hit.edu.cn>
2020-11-17 16:10:02 +0800

    Print dram usage header using the sript provided by the project.

--- dram_consumed.sh
+++ dram_consumed.sh
@@ -9,11 +9,11 @@ cd - > /dev/null
 make 1>&2
 
 sudo dmesg | grep 'Static DRAM usage:' | tail -n 1 | cut -d "]" -f 2 | sed "s/^\s*//g"
-echo size\(GiB\) pmfs_fp_cache_active pmfs_fp_cache_total pmfs_inner_cache0_active pmfs_inner_cache0_total pmfs_inner_cache1_active pmfs_inner_cache1_total pmfs_inner_cache2_active pmfs_inner_cache2_total
+bash ../helper/print_dram_usage_header.sh
 i=1
 while sudo ./write_1G $i 1>&2; do
 	echo -n "$i "
-	bash print_dram_usage.sh
+	bash ../helper/print_dram_usage.sh
 	echo
 	i=$[$i+1]
 done
