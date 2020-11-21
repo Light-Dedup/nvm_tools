5165be25cb4fd931091fc952837e428acc42fc46
Jiansheng QIU <qiujiansheng@stu.hit.edu.cn>
2020-11-21 18:45:49 +0800

    Adaptive size

---
+++ const_size/make_table.sh
@@ -0,0 +1,17 @@
+set -e
+
+if [ ! $3 ]; then
+	echo Usage: $0 max_threads size step_of_dup_rate
+	exit
+fi
+sudo bash -c "echo $0 $* > /dev/kmsg"
+dup_arr=( $(seq 0 $3 100) )
+echo dup_rate numjobs throughput\(GiB/s\)
+cd ..
+for dup in ${dup_arr[@]}; do
+	for i in $(seq 1 $1); do
+		echo -n "$dup $i "
+		bash timing_concurrent.sh $i $2 $dup | grep Total: | sed 's,Total:,,g' | sed 's,GiB/s,,g'
+	done
+done
+
--- make_table.sh
+++ make_table.sh
@@ -1,16 +1,17 @@
 set -e
 
 if [ ! $3 ]; then
-	echo Usage: $0 max_threads size step_of_dup_rate
+	echo Usage: $0 max_threads available_size\(MiB\) step_of_dup_rate
 	exit
 fi
 sudo bash -c "echo $0 $* > /dev/kmsg"
 dup_arr=( $(seq 0 $3 100) )
-echo dup_rate numjobs throughput\(GiB/s\)
+echo dup_rate numjobs each\(MiB\) throughput\(GiB/s\)
 for dup in ${dup_arr[@]}; do
 	for i in $(seq 1 $1); do
-		echo -n "$dup $i "
-		bash timing_concurrent.sh $i $2 $dup | grep Total: | sed 's,Total:,,g' | sed 's,GiB/s,,g'
+		each=$(($2 / $i))
+		echo -n "$dup $i $each "
+		bash timing_concurrent.sh $i ${each}M $dup | grep Total: | sed 's,Total:,,g' | sed 's,GiB/s,,g'
 	done
 done
 
