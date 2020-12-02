56e504bccacd7aa0acc21854163bfb812adeea2f
Jiansheng QIU <632863986@qq.com>
2020-12-02 16:38:31 +0800

    correct.sh now supports multiple test sets.

--- correct.sh
+++ correct.sh
@@ -5,20 +5,18 @@ if [ ! $3 ]; then
 fi
 cd ..
 sudo bash setup.sh
-cd -
-TESTDIR=/home/searchstar/test
-TEST_ARG=$(mktemp)
-echo $* > $TEST_ARG
-if diff $TESTDIR/last_arg.txt $TEST_ARG; then
-	echo The last test has the same arguments, reuse it.
-	rm $TEST_ARG
-else
-	rm -f $TESTDIR/last_arg.txt $TESTDIR/test*
-	TMPFILE=$(mktemp)
+cd - > /dev/null
+
+TESTDIR=$HOME/test/test_$(echo $* | sed 's/ /_/g')
+if [ ! -d $TESTDIR ]; then
+	mkdir -p $TESTDIR
+fi
+cd $TESTDIR
+if [ ! -f complete ]; then
 	for i in $(seq 1 $1); do
 		fio -filename=$TESTDIR/test$i -randseed=$i -direct=1 -iodepth 1 -rw=write -ioengine=psync -bs=4K -thread -numjobs=1 -size=$2 -name=randrw --dedupe_percentage=$3 -group_reporting
 	done
-	mv $TEST_ARG $TESTDIR/last_arg.txt
+	touch complete
 fi
 
 sudo cp $TESTDIR/test* /mnt/pmem/
