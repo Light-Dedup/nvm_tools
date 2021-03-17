594330fb6976d6c55d78a388a1e39c398b6607ed
Jiansheng QIU <632863986@qq.com>
2021-03-17 14:11:46 +0800

    mkstdfile.sh block_size

--- mkstdfile.sh
+++ mkstdfile.sh
@@ -1,8 +1,14 @@
 set -e
 if [ ! $3 ]; then
-	echo Usage: $0 num_of_threads size duplicate_percentage
+	echo Usage: $0 num_of_threads size duplicate_percentage [block_size]
 	exit
 fi
+if [ $4 ]; then
+	bs=$4
+else
+	bs=4K
+fi
+
 TESTDIR=$HOME/fs_test/test_$(echo $* | sed 's/ /_/g')
 if !(cd $TESTDIR 2> /dev/null); then
 	mkdir -p $TESTDIR
@@ -11,10 +17,10 @@ if [ ! -f $TESTDIR/complete ]; then
 	if [[ $1 > 1 && $(echo "$(echo $2 | ./toG) * $1 < $(df -h | grep /mnt/pmem | awk '{print $4}' | ./toG) * 0.8" | bc -l) != 0 ]]; then
     	sudo bash init_ext4.sh /dev/pmem0
 		sudo rm -r /mnt/pmem/*
-		sudo fio -directory=/mnt/pmem -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=4K -thread -numjobs=$1 -size=$2 -name=test --dedupe_percentage=$3 -group_reporting
+		sudo fio -directory=/mnt/pmem -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=$bs -thread -numjobs=$1 -size=$2 -name=test --dedupe_percentage=$3 -group_reporting
 		cp /mnt/pmem/* $TESTDIR
 	else
-		fio -directory=$TESTDIR -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=4K -thread -numjobs=$1 -size=$2 -name=test --dedupe_percentage=$3 -group_reporting
+		fio -directory=$TESTDIR -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=$bs -thread -numjobs=$1 -size=$2 -name=test --dedupe_percentage=$3 -group_reporting
 	fi
 	touch $TESTDIR/complete
 fi
