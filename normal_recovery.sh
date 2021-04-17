e78349ccb48f577c285386f492ff1630bfca1c6f
Jiansheng QIU <632863986@qq.com>
2021-04-17 00:34:13 -0400

    Normal recovery

---
+++ normal_recovery.sh
@@ -0,0 +1,12 @@
+set -e
+cd ..
+make -j$(nproc)
+sudo bash setup.sh 1
+sudo fio -filename=/mnt/pmem/test1 -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=4K -thread -numjobs=1 -size=128G -name=randrw --dedupe_percentage=0 -group_reporting
+sudo bash remount.sh
+sudo gcc ioctl_test.c -o ioctl_test && sudo ./ioctl_test
+dmesg | grep save_refcount | tail -n 1
+dmesg | grep save_entry_allocator | tail -n 1
+dmesg | grep normal_recover_fp_table | tail -n 1
+dmesg | grep normal_recover_entry_allocator | tail -n 1
+
