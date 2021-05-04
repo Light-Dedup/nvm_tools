f9ddc3b57579857a59c6bb4605ba5cf88327eb1a
Jiansheng QIU <632863986@qq.com>
2021-05-04 07:21:52 -0400

    Default dedup

--- zfsdedup/zfsdedup_edonr.sh
+++ zfsdedup/zfsdedup_edonr.sh
@@ -1,4 +1,4 @@
 sudo umount /mnt/pmem
 sudo zpool destroy zfsdedup
 sudo zpool create -m /mnt/pmem zfsdedup /dev/pmem0
-sudo zfs set recordsize=4K dedup=on zfsdedup
+sudo zfs set recordsize=4K dedup=edonr,verify zfsdedup
