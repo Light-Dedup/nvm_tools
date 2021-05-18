b6a224c0dc5575770bdafd274cc8146af1ee7085
Jiansheng QIU <632863986@qq.com>
2021-05-18 12:36:41 -0400

    Measure read/write amount.

---
+++ rw_amount.sh
@@ -0,0 +1,11 @@
+cd ..
+make -j$(nproc)
+sudo bash setup.sh
+cd - > /dev/null
+res1=$(mktemp)
+sudo ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res1
+bash helper/fio.sh $*
+res2=$(mktemp)
+sudo ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res2
+paste $res1 $res2 | awk --non-decimal-data '{print $1,($4-$2)*64}'
+rm $res1 $res2
