d74b188286d28ca192a83eb321fd72db130c00da
Jiansheng QIU <632863986@qq.com>
2021-07-25 08:23:06 -0400

    rw_amount2: First fio, then cp

--- rw_amount2.sh
+++ rw_amount2.sh
@@ -1,25 +1,32 @@
 set -e
 
-if [ ! $1 ]; then
-	echo Usage: $0 size
+if [ ! $2 ]; then
+	echo Usage: $0 threads_first total_size\(MiB\)
 	exit 1
 fi
 
-function run {
-	res1=$(mktemp)
-	sudo ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res1
-	sudo fio -filename=$1 -fallocate=none -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=4K -thread -numjobs=1 -size=$2 -name=test --dedupe_percentage=0 -group_reporting 1>&2
-	res2=$(mktemp)
-	sudo ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res2
-	paste $res1 $res2 | awk --non-decimal-data '{print $1,($4-$2)*64}'
-	rm $res1 $res2
-}
-
 cd ..
-make -j$(nproc) 1>&2
-sudo bash setup.sh 1>&2
+bash setup.sh 1>&2
 cd - > /dev/null
+each=$(($2 / $1))
+
 echo First:
-run /mnt/pmem/test1 $1
+res1=$(mktemp)
+ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res1
+bash helper/fio.sh $1 ${each}M 0 1>&2
+res2=$(mktemp)
+ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res2
+paste $res1 $res2 | awk --non-decimal-data '{print $1,($4-$2)*64}'
+rm $res1 $res2
+
+mkdir /mnt/pmem/0
+mv /mnt/pmem/* /mnt/pmem/0 &> /dev/null || true
+
 echo Second:
-run /mnt/pmem/test2 $1
+res1=$(mktemp)
+ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res1
+cp -r /mnt/pmem/0 /mnt/pmem/1
+res2=$(mktemp)
+ipmctl show -dimm 0x20 -performance | grep TotalMedia | awk -F= '{print $1,$2}' | sed 's/.*Total//g' > $res2
+paste $res1 $res2 | awk --non-decimal-data '{print $1,($4-$2)*64}'
+rm $res1 $res2
