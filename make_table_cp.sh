4326c407aeecb7c11a4d105bb4307ce5568c9ffb
Jiansheng QIU <632863986@qq.com>
2021-08-03 13:47:17 -0400

    mcp

--- make_table_cp.sh
+++ make_table_cp.sh
@@ -1,27 +1,12 @@
 set -e
-if [ ! $2 ]; then
-	echo Usage: $0 max_threads source_dir
+if [ ! $3 ]; then
+	echo Usage: $0 max_threads src_dir dst_dir
 	exit
 fi
 ori=$(pwd)
 echo Threads First\(s\) Second\(s\)
 for i in $(seq 1 $1); do
 	echo -n "$i "
-	cd ..
-	umount /mnt/pmem || true
-	umount /mnt/source || true
-	mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
-	mount -t NOVA -o init -o wprotect,data_cow /dev/pmem1 /mnt/source
-	cd $2
-	#find . -type f -o -type l | $ori/helper/split_dir.py $i /mnt/source
-	find . -type f | $ori/helper/split_dir.py $i /mnt/source
-	cd /mnt/source
-	du -hd 1 1>&2
-	mkdir /mnt/pmem/0
-	/bin/time -f %e bash -c "ls | xargs -P $i -I {} cp -r {} /mnt/pmem/0/" |& xargs echo -n
-	echo -n " "
-	mkdir /mnt/pmem/1
-	cd /mnt/pmem/0
-	/bin/time -f %e bash -c "ls | xargs -P $i -I {} cp -r {} /mnt/pmem/1/" 2>&1
-	cd $ori
+	rm -rf $3
+	( /bin/time -f %e bash -c "ulimit -n 1000000 && mcp $2 $3 $i" 3>&1 1>&2 2>&3 )
 done
