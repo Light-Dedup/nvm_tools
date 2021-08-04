b20654f5767968df230d03a0f3e837a93b5e6ad1
Jiansheng QIU <632863986@qq.com>
2021-08-04 13:10:02 -0400

    sudo bash zfsdedup.sh

--- zfsdedup.sh
+++ zfsdedup.sh
@@ -1,4 +1,9 @@
-sudo umount /mnt/pmem
-sudo zpool destroy zfsdedup
-sudo zpool create -m /mnt/pmem zfsdedup /dev/pmem0
-sudo zfs set recordsize=4K dedup=on zfsdedup
+set -e
+if [ $(whoami) != "root" ]; then
+	echo Please run as root
+	exit
+fi
+umount /mnt/pmem 2> /dev/null || true
+zpool destroy zfsdedup 2> /dev/null || true
+zpool create -m /mnt/pmem zfsdedup /dev/pmem0
+zfs set recordsize=4K dedup=on zfsdedup
