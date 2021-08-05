45765311f4b1d1179ae436a49bed831f7e3df039
csfrogy <854469629@qq.com>
2021-08-05 18:00:40 +0800

    simplify latency tests

--- latency_table.sh
+++ latency_table.sh
@@ -8,7 +8,7 @@ sudo bash -c "echo $0 $* > /dev/kmsg"
 dup_arr=( $(seq 0 $3 100) )
 echo dup_rate numjobs each\(MiB\) latency\(nsec\)
 for dup in ${dup_arr[@]}; do
-	for i in $(seq 1 $1); do
+	for ((i=1;i<=$1;i=i*2));  do
 		each=$(($2 / $i))
 		echo -n "$dup $i $each "
 		bash timing_concurrent.sh $i ${each}M $dup | grep avg= | grep -v clat | awk -F '=' '{print $1" "$2" "$3" "$4" "$5}' | awk '{print $8" "$2}'  | sed 's/,//g' | sed 's/://g'| ./to_nsec
