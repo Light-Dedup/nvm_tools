1b566f15cfa8acfdaf32931b3eda74cb4aabd4e2
csfrogy <854469629@qq.com>
2021-08-10 20:29:53 +0800

    fix two tests

--- make_table.sh
+++ make_table.sh
@@ -8,7 +8,7 @@ sudo bash -c "echo $0 $* > /dev/kmsg"
 dup_arr=( $(seq 0 $3 100) )
 echo dup_rate numjobs each\(MiB\) throughput\(MiB/s\)
 for dup in ${dup_arr[@]}; do
-	for i in $(seq 1 $1); do
+	for ((i=1;i<=$1;i=i*2)); do
 		each=$(($2 / $i))
 		echo -n "$dup $i $each "
 		bash timing_concurrent.sh $i ${each}M $dup | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | ./to_MiB_s
--- make_table2.sh
+++ make_table2.sh
@@ -1,16 +1,19 @@
 set -e
 
-if [ ! $2 ]; then
-	echo Usage: $0 max_threads available_size\(MiB\)
+if [ ! $3 ]; then
+	echo Usage: $0 max_threads available_size\(MiB\) pre_process_path
 	exit
 fi
 sudo bash -c "echo $0 $* > /dev/kmsg"
-for i in $(seq 1 $1); do
+echo Thread Each\(MiB\) First\(MiB/s\) Second\(MiB/s\)
+for ((i=1;i<=$1;i=i*2)); do
 	each=$(($2 / $i))
 	echo -n "$i $each "
-	bash timing_concurrent.sh $i ${each}M 0 > /dev/null
+	bash $3 1>&2
+	bash helper/fio.sh $i ${each}M 0 | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | ./to_MiB_s
 	sudo mkdir /mnt/pmem/0/
 	sudo mv /mnt/pmem/* /mnt/pmem/0/ &> /dev/null || true
-	bash timing_concurrent.sh $i ${each}M 0 | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | ./to_MiB_s
+	echo -n " "
+	bash helper/fio.sh $i ${each}M 0 | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | ./to_MiB_s
 	echo
 done
