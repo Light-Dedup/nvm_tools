d60581ca8fbda7c9b5adefc64e9f14d2af3281fc
deadpool <2841954631@qq.com>
2022-03-08 03:03:11 -0500

    Aging system workload

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CFLAGS := -O3
 
-TARGETS := get_sum toG to_MiB_s exhaust_nvmm write_1G create_files to_nsec
+TARGETS := get_sum toG to_MiB_s exhaust_nvmm write_1G create_files to_nsec aging_system
 all: ${TARGETS}
 
 helper/mt19937ar.o: helper/mt19937ar.c helper/mt19937ar.h
@@ -10,6 +10,9 @@ exhaust_nvmm: helper/exhaust_nvmm.c helper/mt19937ar.o
 	gcc $^ -o $@
 
 write_1G: helper/write_1G.c helper/mt19937ar.o
+	gcc $^ -o $@
+
+aging_system: helper/aging_system.c helper/mt19937ar.o
 	gcc $^ -o $@
 
 %: helper/%.c
---
+++ helper/aging_system.c
@@ -0,0 +1,149 @@
+#define _GNU_SOURCE
+#include "stdio.h"
+#include <unistd.h>
+#include "stdlib.h"
+#include "fcntl.h"
+#include <stdint.h>
+#include "string.h"
+#include "mt19937ar.h"
+#include <errno.h>
+#include <sys/time.h>
+
+extern char *optarg;
+
+void usage() 
+{
+    printf("Aging the system written by deadpool\n");
+    printf("Description: write a file in specific size\n");
+    printf("             punch several holes in the file randomly\n");
+    printf("             write another file to fill the holes\n");
+    printf("-d dir  <dirname>\n");
+    printf("-s size <GiB>\n");
+    printf("-o hole <Percentage(e.g. 50, 60)>\n");
+    printf("Example: ./aging_system -d /mnt/dir -s 50 -o 50\n");
+}
+
+#define BLOCK_SIZE 4096
+#define MAGIC_RAND 2333
+
+void fill_buf(uint32_t buf[]) {
+	int i;
+    for (i = 0; i < BLOCK_SIZE / sizeof(uint32_t); i++) 
+    {
+		buf[i] = genrand_int32();
+	}
+}
+
+double get_ms_diff(struct timeval tvBegin, struct timeval tvEnd)
+{
+    return 1000 * (tvEnd.tv_sec - tvBegin.tv_sec) + ((tvEnd.tv_usec - tvBegin.tv_usec) / 1000.0);
+}
+
+int main(int argc, char const *argv[])
+{
+    char *optstring = "d:s:o:h"; 
+    int opt;
+    int size, hole, hole_num;
+    int fd;
+    int pos;
+    int step = 0, total = 0;
+    int dirlen = 0;
+    char filepath[128] = {0};
+    char buf[BLOCK_SIZE];
+    char *records;
+    struct timeval start, end;
+    double diff; 
+
+    while ((opt = getopt(argc, argv, optstring)) != -1) {
+        switch(opt) {
+        case 'd':
+            dirlen = strlen(optarg);
+            if (dirlen > 1 && optarg[dirlen - 1] == '/') {
+                dirlen -= 1; 
+            }
+            strcpy(filepath, optarg);
+            break;
+        case 's': 
+            size = atoi(optarg);
+            break;
+        case 'o': 
+            hole = atoi(optarg);
+            break;
+        case 'h':
+            usage();
+            exit(1);
+        default:
+            printf("Bad usage!\n");
+            usage();
+            exit(1);
+        }
+    }
+
+    strcpy(filepath + dirlen, "/file1");
+    fd = open(filepath, O_RDWR | O_CREAT);
+    if (fd < 0) {
+        printf("Create file %s error: %s\n", filepath, strerror(errno));
+        exit(1);
+    }
+    
+    total = size * 1024 * 1024 * 1024 / BLOCK_SIZE;
+
+    init_genrand(MAGIC_RAND);
+    
+    printf("Filling file %s with %d blocks...\n", filepath, total);
+    gettimeofday(&start, NULL);
+    for (step = 0; step < total; step++)
+    {
+        fill_buf((uint32_t *)buf);
+        write(fd, buf, BLOCK_SIZE);
+    }
+    gettimeofday(&end, NULL);
+    diff = get_ms_diff(start, end);
+    printf("done, time cost: %.2f ms\n", diff);
+
+    hole_num = total * hole / 100;
+    printf("Bunch %d hole in the %s randomly...\n", hole_num, filepath);
+    records = (char *)malloc(sizeof(char) * total);
+    gettimeofday(&start, NULL);
+    while (hole_num)
+    {
+        pos = (genrand_int32() % total);
+        if (records[pos] == 0)
+        {
+            records[pos] = 1;
+            if(fallocate(fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE, pos * BLOCK_SIZE, BLOCK_SIZE) < 0)
+            {
+                perror("could not deallocate");
+            }
+            
+            hole_num--;
+        }
+    }
+    gettimeofday(&end, NULL);
+    diff = get_ms_diff(start, end);
+    printf("done, time cost: %.2f ms\n", diff);
+    free(records);
+    close(fd);
+
+    strcpy(filepath + dirlen, "/file2");
+    printf("Filling holes by %s with %d blocks...\n", filepath, total);
+    fd = open(filepath, O_RDWR | O_CREAT);
+    if (fd < 0) {
+        printf("Create file %s error: %s\n", filepath, strerror(errno));
+        exit(1);
+    }
+    hole_num = total * hole / 100;
+    gettimeofday(&start, NULL);
+    while (hole_num)
+    {
+        fill_buf((uint32_t *)buf);
+        write(fd, buf, BLOCK_SIZE);
+        hole_num--;
+    }    
+    gettimeofday(&end, NULL);
+    diff = get_ms_diff(start, end);
+    printf("done, time cost: %.2f ms\n", diff);
+    close(fd);
+
+    return 0;
+}
