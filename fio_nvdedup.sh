f318557336bb52c5b3f2dc7c4ded8db917181c2b
deadpool <2841954631@qq.com>
2022-05-31 11:24:27 +0800

    add nv-dedup setups related scripts

---
+++ fio_nvdedup.sh
@@ -0,0 +1,41 @@
+if [ ! $3 ]; then
+	echo Usage: $0 num_of_threads size dup_rate branch_name measure_timing
+	exit 1
+fi
+
+ABSPATH=$(cd "$( dirname "$0" )" && pwd)
+
+function set_pmem () {
+    local script=$1
+    local pmem=$2
+    
+    cp "$script" "/tmp/setups" -f
+    sed -i "s/pmem0/pmem/g" "$script"
+    sed -i "s/pmem/$pmem/g" "$script"
+}
+
+function restore_pmem () {
+    local script=$1
+    
+    cp "/tmp/setups" "$script" -f
+}
+
+
+branch_name=$4
+measure_timing=$5
+
+cd "$ABSPATH"/../nv-dedup/ || exit
+
+git checkout "$branch_name"
+sudo make -j32
+sudo bash -c "echo $0 $* > /dev/kmsg"
+
+restore_pmem "setup.sh" 
+set_pmem "setup.sh" "pmem1"
+sudo bash setup.sh "$measure_timing"
+restore_pmem "setup.sh" 
+git checkout -- "setup.sh"
+
+cd - || exit
+
+bash "$ABSPATH"/helper/fio.sh $*
---
+++ setup_nvdedup.sh
@@ -0,0 +1,36 @@
+if [ ! "$1" ]; then
+	echo Usage: "$0" branch_name measure_timing
+	exit 1
+fi
+
+ABSPATH=$(cd "$( dirname "$0" )" && pwd)
+
+branch_name=$1
+
+function set_pmem () {
+    local script=$1
+    local pmem=$2
+    
+    cp "$script" "/tmp/setups" -f
+    sed -i "s/pmem0/pmem/g" "$script"
+    sed -i "s/pmem/$pmem/g" "$script"
+}
+
+function restore_pmem () {
+    local script=$1
+    
+    cp "/tmp/setups" "$script" -f
+}
+
+
+cd "$ABSPATH"/../nv-dedup/ || exit
+git checkout "$branch_name"
+sudo make -j32
+sudo bash -c "echo $0 $* > /dev/kmsg"
+
+restore_pmem "setup.sh" 
+set_pmem "setup.sh" "pmem1"
+sudo bash setup.sh "$2"
+restore_pmem "setup.sh" 
+git checkout -- "setup.sh"
+cd - || exit
